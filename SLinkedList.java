4136364eff92c526a1c828f51a61e231f2f13e60
Beatlord <berner4r@gmail.com>
2026-02-17 12:14:46 +0200

    First Commit

--- SLinkedList.java
+++ SLinkedList.java
@@ -47,8 +47,10 @@ public class SLinkedList<T> implements Iterable<T>{
     public void addFirst(T data){
         Node<T> newNode = new Node<>(data);
         newNode.next = head;
+        if (head == null) {
+            tail = newNode; // Only update tail if it's the very first element
+            }
         head = newNode;
-        tail=newNode;
         size++;
     }
     public void addLast(T data){
@@ -136,17 +138,58 @@ public class SLinkedList<T> implements Iterable<T>{
         return tail.data;
     }
     public void reverse(){
-        
-    //TODO
+        if (head == null || head.next == null) return;
+        Node<T> prev = null;
+        Node<T> current = head;
+        Node<T> nextNode = null;
+        // Update tail to be the current head
+        tail = head;
+        while (current != null) {
+            nextNode = current.next; // Store the next node
+            current.next = prev;     // Flip the pointer backward
+            prev = current;          // Move prev forward
+            current = nextNode;      // Move current forward
+            }
+        head = prev; // The last 'prev' becomes the new head        
+   
     }
     public void deleteConsecutiveDuplicates(){
-       //TODO
+        if (head == null) return;
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.data.equals(current.next.data)) {
+                // Skip the next node
+                current.next = current.next.next;
+                size--;
+                // Update tail if we just deleted the end
+                if (current.next == null) {
+                    tail = current;
+            }} else {
+                current = current.next; // Only move forward if no deletion
+        }
     }
+    }
+
     //two lists are equal if they have the same 
     // size and the same elements in the same order
     @Override
     public boolean equals(Object obj){
-        //TODO
+        if (this == obj) return true;
+        if (!(obj instanceof SLinkedList)) return false;
+
+        SLinkedList<T> other = (SLinkedList<T>) obj;
+        if (this.size != other.size) return false;
+
+        Node<T> currentThis = this.head;
+        Node<T> currentOther = other.head;
+        while (currentThis != null) {
+            if (!currentThis.data.equals(currentOther.data)) {
+                return false;
+        }
+            currentThis = currentThis.next;
+            currentOther = currentOther.next;
+    }
+        return true;
     }
 
 }
